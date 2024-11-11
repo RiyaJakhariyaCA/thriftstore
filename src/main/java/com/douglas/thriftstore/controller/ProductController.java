6ef7ddbf43d7853cc68423795ae3fe9fb7202448
RiyaJakhariyaCA <jakhariyar@student.douglascollege.ca>
2024-11-10 22:39:44 -0800

    Update ProductController.java

--- src/main/java/com/douglas/thriftstore/controller/ProductController.java
+++ src/main/java/com/douglas/thriftstore/controller/ProductController.java
@@ -11,6 +11,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin("http://localhost:8080/")
 @RestController
 @RequestMapping("/api/products")
 public class ProductController {
