06c5d7aab0c7949e8115008ad5b0210ca61e838b
RiyaJakhariyaCA <jakhariyar@student.douglascollege.ca>
2024-11-10 22:39:56 -0800

    Update UserController.java

--- src/main/java/com/douglas/thriftstore/controller/UserController.java
+++ src/main/java/com/douglas/thriftstore/controller/UserController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin("http://localhost:8080/")
 @RestController
 @RequestMapping("/users")
 public class UserController {
